f09dc285f3487df5a1fdcda39e3af685c2cf01a8
nou3584 <wuneng3@gmail.com>
2026-05-27 01:57:22 +0900

    Update status.php

--- api/status.php
+++ api/status.php
@@ -8,11 +8,11 @@
 header('Content-Type: application/json; charset=utf-8');
 
 $response = [
-  "site" => "KINGDOM 3584",
+  "site" => "KINGDOM 3584 - Saint Empire",
   "owner" => "nou3584",
   "repository" => "kd3584",
   "github_pages_url" => "https://nou3584.github.io/kd3584/",
-  "discord" => "https://discord.gg/7rYzwV5ga",
+  "discord" => "https://discord.gg/QA2q6GEq8",
   "status" => "ok",
   "message" => "Applications are reviewed by kingdom leadership."
 ];
