b361ff8729342326263ea620736907ddcd93c9ec
nou3584 <wuneng3@gmail.com>
2026-05-27 01:22:39 +0900

    Create status.php

---
+++ api/status.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Optional PHP endpoint.
+ * GitHub Pages cannot execute PHP.
+ * Use this file only on Sakura Rental Server or another PHP-compatible server.
+ */
+
+header('Content-Type: application/json; charset=utf-8');
+
+$response = [
+  "site" => "KINGDOM 3584",
+  "owner" => "nou3584",
+  "repository" => "kd3584",
+  "github_pages_url" => "https://nou3584.github.io/kd3584/",
+  "discord" => "https://discord.gg/7rYzwV5ga",
+  "status" => "ok",
+  "message" => "Applications are reviewed by kingdom leadership."
+];
+
+echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+?>
